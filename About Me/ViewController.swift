e50d2bf750b0bdf907ed40699b707f4c38a4fc88
Louis Baez <shark2003247543@gmail.com>
2021-04-28 11:34:54 -0400

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Louis Baez  on 4/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
